75419068ec6de8c0d2facc5a0aae86e357121a55
0x25CBFC4F <me@voids.site>
2025-10-05 20:14:34 +0400

    Add migration to enforce unique constraint on `Username` in `users` table

---
+++ NullOps/DAL/Migrations/20251005154353_Fixed Users constraint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NullOps.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedUsersconstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Username",
+                table: "users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_Username",
+                table: "users");
+        }
+    }
+}
