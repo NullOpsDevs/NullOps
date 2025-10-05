f5cdb85517e50120a18fad788f483ae04b278055
0x25CBFC4F <me@voids.site>
2025-10-05 14:31:33 +0400

    Add `Settings` migration with table definition and indexes

---
+++ NullOps/DAL/Migrations/20250928112814_User indexes, IsBlocked and time fields.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NullOps.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserindexesIsBlockedandtimefields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBlocked",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_CreatedAt",
+                table: "users",
+                column: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_CreatedAt",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "IsBlocked",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "users");
+        }
+    }
+}
---
+++ NullOps/DAL/Migrations/20250928113258_Added Settings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NullOps.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "settings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Key = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Value = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_settings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_settings_CreatedAt",
+                table: "settings",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_settings_Key",
+                table: "settings",
+                column: "Key");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "settings");
+        }
+    }
+}
