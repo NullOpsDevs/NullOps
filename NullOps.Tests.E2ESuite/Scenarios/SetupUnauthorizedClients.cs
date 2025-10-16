82d7a5fe1a7b90fae143f5587ee344868732dc60
ht <HelenaTalanova.G@gmail.com>
2025-10-16 19:13:41 +0300

    Add unauthorized scenarios users get me and get users

---
+++ NullOps.Tests.E2ESuite/Scenarios/SetupUnauthorizedClients.cs
@@ -0,0 +1,24 @@
+﻿using NullOps.Tests.E2ESuite.Clients;
+using Refit;
+
+namespace NullOps.Tests.E2ESuite.Scenarios;
+
+public class SetupUnauthorizedClients : Scenario<GlobalTestContext>
+{
+    public SetupUnauthorizedClients() : base("Setup API unauthorized clients")
+    {
+        AddStep("Setting up API unauthorized clients for next scenarios", SetUnauthorizedClientsForNextScenarios);
+    }
+    
+    private static Task SetUnauthorizedClientsForNextScenarios(GlobalTestContext ctx)
+    {
+        var refitSettings = new RefitSettings
+        {
+            AuthorizationHeaderValueGetter = (_, _) => Task.FromResult(Guid.NewGuid().ToString())
+        };
+        
+        ctx.UsersClient = RestService.For<IUsersClient>(ctx.BaseUrl, refitSettings);
+        
+        return Task.CompletedTask;
+    }
+}
---
+++ NullOps.Tests.E2ESuite/Scenarios/UnauthorizedUsersScenario.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace NullOps.Tests.E2ESuite.Scenarios;
+
+public class UnauthorizedUsersScenario : Scenario<GlobalTestContext>
+{
+    public UnauthorizedUsersScenario() : base("Verify unauthorized")
+    {
+        AddStep("Verify unauthorized get me", VerifyUnauthorizedGetMe);
+        AddStep("Verify unauthorized get users", VerifyUnauthorizedGetUsers);
+    }
+    
+    private static async Task VerifyUnauthorizedGetMe(GlobalTestContext ctx)
+    {
+        var response = await ctx.UsersClient.GetMeAsync();
+        
+        Assert.ExpectStatusCode(response, HttpStatusCode.Unauthorized);
+    }
+    
+    private static async Task VerifyUnauthorizedGetUsers(GlobalTestContext ctx)
+    {
+        var response = await ctx.UsersClient.GetUsersAsync();
+        
+        Assert.ExpectStatusCode(response, HttpStatusCode.Unauthorized);
+    }
+}
